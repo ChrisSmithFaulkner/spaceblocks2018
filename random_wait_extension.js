712a51b5bffcdc9fa1f60ed943254791531f9397
ChrisSmithFaulkner <christopher.smith@faulkner.edu>
2018-10-20 18:56:55 -0500

    Test api call

--- random_wait_extension.js
+++ random_wait_extension.js
@@ -23,12 +23,39 @@ new (function() {
             callback();
         }, wait*1000);
     };
+	
+	ext.power_temp_request = function(lat,lon,) {
+		//create POWER url	
+		url = 'https://power.larc.nasa.gov/cgi-bin/v1/DataAccess.py?request=execute&identifier=SinglePoint&parameters=T2M&startDate=20160301&endDate=20160331&userCommunity=SSE&tempAverage=DAILY&outputList=JSON,ASCII&lat='+lat+'&lon='+lon+'&user=anonymous'
+
+		//request data
+		$.getJSON( url, function( data ) { 
+
+			  }).done( function ( data ) {
+
+					//access the data values within the json data response 
+					features = data["features"];
+					properties = features[0].properties;
+					parameter = properties.parameter;
+
+					for ( var p in parameter ) {
+						  var values = features[0].properties.parameter[p];
+						  for ( var v in values ){	
+								var powervalue = values[v];
+						  }
+		});
+		});
+	};
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['w', 'wait for random time', 'wait_random'],
-        ]
+			['R', 'execute POWER API call for temperature', 'power_temp_request',36,45],
+        ],
+		menus: {
+			item: ["Title", "URL", "Content"]
+		}
     };
 
     // Register the extension
