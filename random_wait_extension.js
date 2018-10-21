40a7051c9e39df2193b6c19585d5156a88a9ab5c
ChrisSmithFaulkner <35348066+ChrisSmithFaulkner@users.noreply.github.com>
2018-10-21 08:01:06 -0500

    testing

--- random_wait_extension.js
+++ random_wait_extension.js
@@ -9,19 +9,7 @@
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
     };
-     // Functions for block with type 'w' will get a callback function as the 
-    // final argument. This should be called to indicate that the block can
-    // stop waiting.
-    ext.wait_random = function(callback) {
-        wait = Math.random();
-        console.log('Waiting for ' + wait + ' seconds');
-        window.setTimeout(function() {
-            callback();
-        }, wait*1000);
-    };
-	ext.get_temp_data = function(callback) {
-		callback();
-	};
+     
 	ext.get_temp = function(callback) {
         // Make an AJAX call to the Open Weather Maps API
         $.ajax({
@@ -44,9 +32,7 @@
      // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            ['w', 'wait for random time', 'wait_random'],
-			['W', 'return temp value', 'get_temp_data'],
-			['R', 'current temperature', 'get_temp'],
+            ['R', 'current temperature', 'get_temp'],
 		
         ]
     };
